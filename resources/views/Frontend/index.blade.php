c10fc2ebd84dda537957a1a5473a42700c1bf3d1
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-19 22:08:56 +0500

    c
    c

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -161,41 +161,40 @@
         <div class="grid" id="productGrid1">
             <!-- Example Card -->
             @foreach ($ads as $ad)
-                <div class="card">
-                    @php
-                        $images = json_decode($ad->images, true);
-                    @endphp
-
+              <div class="card">
+                <div class="image-box">
                     @if (is_array($images) && isset($images[0]))
-                        <img src="{{ asset('' . $images[0]) }}" alt="Product">
+                        <img src="{{ asset($images[0]) }}" alt="Product">
                     @endif
-                    <div class="card-body">
-                        <a href="{{ route('view.ad', ['slug' => Str::slug($ad->title), 'id' => $ad->id]) }}"
-                            class="card-title">{{ $ad->title }}</a>
-                        <div class="price">AED {{ $ad->price }}</div>
-                        <div class="meta">
-                            Availability: In Stock <br>
-                            Condition: {{ $ad->condition }} <br>
-                            Delivery: Ask Supplier <br>
-                            Warranty: Ask Supplier
-                        </div>
-                        @php
-                            $ad->shop->supplier->whatsapp;
-                        @endphp
-                        <div class="buttons">
-                            <a href="javascript:void(0)" class="btn whatsapp"
-                                onclick="contactSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}', '{{ $ad->title }}')">
-                                <i class="fa-brands fa-whatsapp"></i> WhatsApp
-                            </a>
+                </div>
 
-                            <a href="javascript:void(0)" class="btn call"
-                                onclick="callSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}')">
-                                <i class="fa-solid fa-phone"></i> Click to Call
-                            </a>
-                        </div>
+                <div class="card-body">
+                    <a href="{{ route('view.ad', ['slug' => Str::slug($ad->title), 'id' => $ad->id]) }}"
+                        class="card-title">{{ $ad->title }}</a>
 
+                    <div class="price">AED {{ $ad->price }}</div>
+
+                    <div class="meta">
+                        Availability: In Stock <br>
+                        Condition: {{ $ad->condition }} <br>
+                        Delivery: Ask Supplier <br>
+                        Warranty: Ask Supplier
+                    </div>
+
+                    <div class="buttons">
+                        <a href="javascript:void(0)" class="btn whatsapp"
+                            onclick="contactSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}', '{{ $ad->title }}')">
+                            <i class="fa-brands fa-whatsapp"></i> WhatsApp
+                        </a>
+
+                        <a href="javascript:void(0)" class="btn call"
+                            onclick="callSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}')">
+                            <i class="fa-solid fa-phone"></i> Click to Call
+                        </a>
                     </div>
                 </div>
+            </div>
+
             @endforeach
 
             {{-- <div class="card">
