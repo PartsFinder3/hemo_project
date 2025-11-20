86bf10eaf92489b2b7acb23a069d97545e38b073
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 14:55:35 +0500

    c
    c

--- resources/views/supplierPanel/shopProfile/view.blade.php
+++ resources/views/supplierPanel/shopProfile/view.blade.php
@@ -189,6 +189,7 @@
                                             @endphp
 
                                             @if (is_array($images) && isset($images[0]))
+                                            {{$images[0]}}
                                                 <img src="{{ asset('storage/' . $images[0]) }}"
                                                     class="card-img-top img-fluid" alt="Product">
                                             @endif
