0cbdac23998c0134c9e461078b1035a749033c87
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 12:43:01 +0500

    c
    c

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -247,7 +247,7 @@
                         href="{{ route('part.ads', ['partName' => Str::slug($p->name), 'id' => $p->id]) }}">
                 <div class="part-card">
                     @if ($p->image)
-                 {{$p->image}}
+             
                       <img src="{{ asset('storage/' . $p->image) }}" alt="Spare Part" >
                     @else
                         <img src="{{ asset('Frontend/assets/quote.png') }}" alt="{{ $p->name }}" />
