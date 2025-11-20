fe63a498e17f32ca803471d08238fadfd6799e8c
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 12:47:18 +0500

    c
    c

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -137,7 +137,7 @@
                 <a href="{{ route('make.ads', ['slug' => $make->slug, 'id' => $make->id]) }}" class="make">
                     @if($make->logo)
                    {{$make->logo}}
-                        <img src="{{ asset('storage/' . $p->image) }}" alt="{{ $make->name }}">
+                        <img src="{{ asset('storage/' . $make->logo) }}" alt="{{ $make->name }}">
                     @endif
                     <h4>{{ strtoupper($make->name) }}</h4>
                 </a>
