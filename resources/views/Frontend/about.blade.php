260f217108bb2ff3f6d8f39c13a95f8f03bf3c4d
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 22:15:17 +0500

    c
    c

--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -1,38 +1,6 @@
 @extends('frontend.layout.main')
 @section('main-section')
-    <div class="hero-section">
-        <div class="hero-text d-flex justify-content-center align-items-center flex-column">
-       
-            </span>
-            <style>
-                .scroll-bounce {
-                    display: inline-block;
-                    animation: bounce 1.4s infinite;
-                }
-
-                @keyframes bounce {
-
-                    0%,
-                    20%,
-                    50%,
-                    80%,
-                    100% {
-                        transform: translateY(0);
-                    }
-
-                    40% {
-                        transform: translateY(6px);
-                    }
-
-                    60% {
-                        transform: translateY(3px);
-                    }
-                }
-            </style>
-
-            {{-- <p>Your one-stop solution for all your automotive needs.</p> --}}
-        </div>
-    </div>
+ 
     </main>
 @if($domain)
     {!! $domain->about !!}
