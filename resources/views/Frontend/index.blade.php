8b610c42e1f5d175d0097dc0c8557b3d775d226e
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 20:03:27 +0500

    c
    c

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -248,7 +248,7 @@
                 <div class="part-card">
                     @if ($p->image)
                         {{$p->image}}
-                      <img src="{{ asset('storage/' . $p->image) }}" alt="Spare Part" >
+                      <img src="{{ asset( $p->image) }}" alt="Spare Part" >
                     @else
                         <img src="{{ asset('Frontend/assets/quote.png') }}" alt="{{ $p->name }}" />
                     @endif
