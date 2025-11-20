fff947264a5fbf279403a80eadaeb204480e43cf
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 22:27:12 +0500

    c
    c

--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -1,10 +1,17 @@
 @extends('frontend.layout.main')
-@section
- 
+@section('main-section')
+
+<style>
+    .hero-section {
+        background: none !important;
+        height: auto !important;
+    }
+</style>
 
 @if($domain)
     {!! $domain->about !!}
 @else
     <p>No domain configuration found.</p>
-@endif
+@endif  
+
 @endsection
