2a229842ab2c0d7173d326114e07842f32dcbc2a
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 22:23:33 +0500

    c
    c

--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -1,7 +1,16 @@
 @extends('frontend.layout.main')
 @section('main-section')
  
-
+<style>
+    main {
+        display: none;
+    max-width: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0.342), rgba(0, 0, 0, 0.8)), url(../assets/Untitled design (2).png);
+    min-height: 100vh;
+    background-size: cover;
+    background-position: center;
+}
+</style>
 @if($domain)
     {!! $domain->about !!}
 @else
