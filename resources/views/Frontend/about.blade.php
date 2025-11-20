8574ad72b29177d2f09b756a0cf0512cb7cd6e34
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 22:28:53 +0500

    c
    c

--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -1,17 +1,19 @@
 @extends('frontend.layout.main')
 @section('main-section')
-
+ 
 <style>
-    .hero-section {
-        background: none !important;
-        height: auto !important;
-    }
+    main {
+     
+    max-width: 100%;
+    background: linear-gradient(rgba(255, 0, 0, 0.342), rgba(0, 0, 0, 0.8)), url(../assets/Untitled design (2).png);
+    min-height: 100vh;
+    background-size: cover;
+    background-position: center;
+}
 </style>
-
 @if($domain)
     {!! $domain->about !!}
 @else
     <p>No domain configuration found.</p>
-@endif  
-
+@endif
 @endsection
