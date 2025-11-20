ecbc8a46f50ba4d6f9f4433837286153fb4dcb47
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-20 22:44:04 +0500

    c
    c

--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -1,9 +1,11 @@
-@extends('frontend.layout.main')
+@include('frontend.layout.head')
+
 @section('main-section')
-   
-   @if($domain)
+
+@if($domain)
     {!! $domain->about !!}
 @else
     <p>No domain configuration found.</p>
 @endif
+
 @endsection
