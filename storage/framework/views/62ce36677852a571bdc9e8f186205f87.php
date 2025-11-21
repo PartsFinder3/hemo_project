4e876cfa8fe5c6cc918128bb71f5c41485171471
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-21 08:43:43 +0500

    c
    c

--- storage/framework/views/62ce36677852a571bdc9e8f186205f87.php
+++ storage/framework/views/62ce36677852a571bdc9e8f186205f87.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>- Dashboard</title>
+    <title><?php echo e($domain->name); ?> - Dashboard</title>
 
     
     <link rel="shortcut icon" href="<?php echo e(asset('assets/compiled/svg/favicon.svg')); ?>" type="image/x-icon">
