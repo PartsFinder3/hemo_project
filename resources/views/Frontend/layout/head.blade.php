0b1a2a913dbaa8f11fdef019780a0f1afbea8dac
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-22 19:57:23 +0500

    m

--- resources/views/Frontend/layout/head.blade.php
+++ resources/views/Frontend/layout/head.blade.php
@@ -50,7 +50,8 @@
                                 #w-left{
                                     display: none
                                 }
-                            }
+             
+                                }
     </style> --}}
         <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
