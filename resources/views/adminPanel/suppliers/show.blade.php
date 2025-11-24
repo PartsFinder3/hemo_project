df48e44f3daddde58a40616fd32e65afb07d919f
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-24 16:35:50 +0500

    c

--- resources/views/adminPanel/suppliers/show.blade.php
+++ resources/views/adminPanel/suppliers/show.blade.php
@@ -78,15 +78,17 @@
                                     <td>{{ $supplier->city->name }}</td>
                                     <td>{{ $supplier->whatsapp }}</td>
                                     @if (auth()->guard('admins')->user()->role == 'admin')
-                                        <td>
+                                      <td>
                                             @if ($supplier->is_active)
-                                                <a href="{{ route('suppliers.active.toggle', $supplier->id) }}"
-                                                    class="btn btn-warning">Deactivate</a>
+                                                <a href="{{ route('suppliers.active.toggle', $supplier->id) }}" class="btn btn-warning">
+                                                    <i class="fa fa-check"></i> <!-- Active: check mark -->
+                                                </a>
                                             @else
-                                                <a href="{{ route('suppliers.active.toggle', $supplier->id) }}"
-                                                    class="btn btn-success">Activate</a>
+                                                <a href="{{ route('suppliers.active.toggle', $supplier->id) }}" class="btn btn-secondary">
+                                                    <i class="fa fa-times"></i> <!-- Inactive: cross mark -->
+                                                </a>
                                             @endif
-                                        </td>
+
                                         <td>
                                             @if ($supplier->is_verified)
                                                 <a href="{{ route('suppliers.verified.toggle', $supplier->id) }}"
