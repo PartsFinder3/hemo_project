d12aab13a83c19f370abb6641de2d64962c5464e
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-24 19:14:52 +0500

    c

--- resources/views/Frontend/view-add.blade.php
+++ resources/views/Frontend/view-add.blade.php
@@ -117,9 +117,12 @@
         }
         .fa-whatsapp:before{
             font-size: 50px;
+            color: green
         }
         .fa-phone:before{
             font-size: 50px;
+            padding-left: 40px;
+            color: #e00;
         }
     </style>
 </head>
