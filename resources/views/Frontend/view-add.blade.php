1bca18224ab0653c8e8712fea1098bf321be5e11
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-24 19:07:22 +0500

    c

--- resources/views/Frontend/view-add.blade.php
+++ resources/views/Frontend/view-add.blade.php
@@ -170,15 +170,25 @@
                 <p class="price">AED: {{ $ad->price }}</p>
 
                 <div class="d-flex gap-2 my-3">
-                    <a href="javascript:void(0)" class="btn whatsapp"
-                        onclick="contactSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}', '{{ $ad->title }}')">
-                        <i class="fa-brands fa-whatsapp"></i> WhatsApp
-                    </a>
+<a href="javascript:void(0)" class="btn whatsapp"
+    onclick="contactSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}', '{{ $ad->title }}')">
 
-                    <a href="javascript:void(0)" class="btn call"
-                        onclick="callSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}')">
-                        <i class="fa-solid fa-phone"></i> Click to Call
-                    </a>
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24">
+        <path d="M12.04 2c-5.52 0-10 4.48-10 10 0 1.76.46 3.47 1.34 5L2 22l5.19-1.36c1.45.79 3.08 1.21 4.85 1.21h.01c5.52 0 10-4.48 10-10s-4.48-10-10-10zm5.8 14.48c-.24.68-1.4 1.32-1.93 1.38-.49.05-1.1.07-1.78-.11-.41-.11-.94-.3-1.62-.59-2.84-1.24-4.69-4.15-4.83-4.34-.14-.19-1.15-1.53-1.15-2.92 0-1.39.73-2.07 1-2.36.27-.29.59-.36.79-.36h.57c.18 0 .43-.07.67.51.24.58.82 2.01.9 2.16.07.15.12.32.02.51-.1.19-.15.31-.3.48-.15.17-.31.38-.44.51-.15.15-.31.32-.13.63.18.31.81 1.34 1.74 2.17 1.2 1.07 2.21 1.41 2.52 1.57.31.16.49.13.67-.08.18-.21.77-.9.98-1.21.21-.31.41-.25.68-.15.27.1 1.72.81 2.02.96.3.15.5.22.57.34.07.12.07.71-.17 1.39z"/>
+    </svg>
+
+    WhatsApp
+</a>
+
+<a href="javascript:void(0)" class="btn call"
+    onclick="callSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}')">
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24">
+        <path d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2a1 1 0 011.01-.24 11.72 11.72 0 003.67.59 1 1 0 011 1v3.5a1 1 0 01-1 1A17.5 17.5 0 012 6a1 1 0 011-1h3.5a1 1 0 011 1 11.73 11.73 0 00.59 3.67 1 1 0 01-.24 1.01l-2.23 2.11z"/>
+    </svg>
+
+    Click to Call
+</a>
                 </div>
 
                 {{-- <p class="text-success fw-semibold">⭐ 98.9% Positive Feedback</p> --}}
