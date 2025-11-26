c38e6ae5eaec6c0e99dd4d76d4972860d10b14ef
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-11-26 13:07:15 +0500

    c

--- resources/views/adminPanel/layout/head.blade.php
+++ resources/views/adminPanel/layout/head.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> - Dashboard</title>
+    <title>{{ $domain->name }} - Dashboard</title>
 
     {{-- Favicons --}}
     <link rel="shortcut icon" href="{{ asset('assets/compiled/svg/favicon.svg') }}" type="image/x-icon">
