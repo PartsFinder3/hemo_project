1720b5fa38d5fd5f19cce239a0f5c5f7a961d1d2
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-01 19:31:01 +0500

    c

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -356,83 +356,48 @@
         </div>
 
         <div class="grid" id="productGrid1">
-            <!-- Example Card -->
             @foreach ($ads as $ad)
                 <div class="card">
-                    @php
-                        $images = json_decode($ad->images, true);
-                    @endphp
+                    @php $images = json_decode($ad->images, true); @endphp
 
-                         @if(!empty($images[0]))
-                                            <img src="{{ asset($images[0]) }}" class="card-img-top img-fluid" alt="Product">
-               @endif
+                    @if(!empty($images[0]))
+                        <img src="{{ asset($images[0]) }}" class="card-img-top img-fluid" alt="Product">
+                    @endif
+
                     <div class="card-body">
                         <a href="{{ route('view.ad', ['slug' => Str::slug($ad->title), 'id' => $ad->id]) }}"
                             class="card-title">{{ $ad->title }}</a>
+
                         <div class="price">{{ $ad->currency }} {{ $ad->price }}</div>
+
                         <div class="meta">
                             Availability: In Stock <br>
                             Condition: {{ $ad->condition }} <br>
                             Delivery: Ask Supplier <br>
                             Warranty: Ask Supplier
                         </div>
-                        @php
-                             $ad->shop->supplier->whatsapp;
-                        @endphp
+
                         <div class="buttons">
-                   
-                                <a href="https://wa.me/{{ preg_replace('/\D/', '', $ad->shop->supplier->whatsapp) }}?text={{ urlencode('Hello, I am interested in your ad: ' . $ad->title) }}"
+                            <a href="https://wa.me/{{ preg_replace('/\D/', '', $ad->shop->supplier->whatsapp) }}?text={{ urlencode('Hello, I am interested in your ad: ' . $ad->title) }}"
                                 target="_blank"
                                 class="btn btn-sm btn-success w-100 my-1">
-                                    <i class="fab fa-whatsapp me-1"></i> WhatsApp
-                                </a>
+                                <i class="fab fa-whatsapp me-1"></i> WhatsApp
+                            </a>
+
                             <a href="javascript:void(0)" class="btn call"
                                 onclick="callSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}')">
                                 <i class="fa-solid fa-phone"></i> Click to Call
                             </a>
                         </div>
-
                     </div>
                 </div>
             @endforeach
-
-            {{-- <div class="card">
-                <img src="assets/ad (2).jpg" alt="Product">
-                <div class="card-body">
-                    <a href="" class="card-title">Mercedes Cla200 2019 Window Switch Panel</a>
-                    <div class="price">AED 100</div>
-                    <div class="meta">
-                        Availability: In Stock <br>
-                        Condition: Used <br>
-                        Delivery: Ask Supplier <br>
-                        Warranty: Ask Supplier
-                    </div>
-                    <div class="buttons">
-                        <a href="#" class="btn whatsapp">WhatsApp</a>
-                        <a href="#" class="btn call">Click to Call</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <img src="assets/ad (2).jpg" alt="Product">
-                <div class="card-body">
-                    <a href="" class="card-title">Mercedes Cla200 2019 Window Switch Panel</a>
-                    <div class="price">AED 100</div>
-                    <div class="meta">
-                        Availability: In Stock <br>
-                        Condition: Used <br>
-                        Delivery: Ask Supplier <br>
-                        Warranty: Ask Supplier
-                    </div>
-                    <div class="buttons">
-                        <a href="#" class="btn whatsapp">WhatsApp</a>
-                        <a href="#" class="btn call">Click to Call</a>
-                    </div>
-                </div>
-            </div> --}}
-
-            <!-- Repeat similar cards... -->
         </div>
+
+<!-- Pagination -->
+<div class="mt-4">
+    {{ $ads->links('pagination::bootstrap-5') }}
+</div>
 
         <div class="pagination" id="pagination1"></div>
     </section>
