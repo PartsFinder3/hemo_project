564adb9c84bd9b0fb68c2ab7fc17ce2df15a533e
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-06 11:23:54 +0500

    c

--- resources/views/Frontend/hero_section.blade.php
+++ resources/views/Frontend/hero_section.blade.php
@@ -63,7 +63,7 @@
     width: 100%;
     display: flex;
     flex-direction: row;
-    /* justify-content: space-between; */
+    justify-content: space-between;
     /* align-items: center; */
       justify-content: center; 
     padding: 0 50px;
