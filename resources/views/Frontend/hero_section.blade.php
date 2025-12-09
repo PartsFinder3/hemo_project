8e04275810ff237d430d674ece6f7e503ffb8f5d
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-09 21:20:15 +0500

    c

--- resources/views/Frontend/hero_section.blade.php
+++ resources/views/Frontend/hero_section.blade.php
@@ -1,3 +1,6 @@
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <style>
 /* ===== Hero Section ===== */
@@ -284,7 +287,6 @@
     outline: 0;
     height: auto;
 }
-
 </style>
 
 <div class="hero_section_text">
