c4b8ef9529e16cd0f7d92cac1412e2242c00fd18
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-09 14:56:51 +0500

    c

--- resources/views/Frontend/layout/footer.blade.php
+++ resources/views/Frontend/layout/footer.blade.php
@@ -72,7 +72,7 @@
             </div>
         </div>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
         window.getModelsUrl = "{{ url('get-models') }}/";
