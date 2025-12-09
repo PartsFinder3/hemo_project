6fb806ef0f01157f2aaff16251a712c192d4953b
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-09 21:48:25 +0500

    c

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,3 @@ use App\Http\Controllers\ApiController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/get_numbers',[ApiController::class,'numbers'])->name('api.numbers');
