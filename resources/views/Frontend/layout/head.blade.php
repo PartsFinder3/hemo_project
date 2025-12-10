fe6c2929235fc0da9215dabd6bcfda8658a48f5e
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-10 19:14:29 +0500

    c

--- resources/views/Frontend/layout/head.blade.php
+++ resources/views/Frontend/layout/head.blade.php
@@ -29,6 +29,12 @@
 <meta property="og:image:height" content="945">
 @endif
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Choices.js CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
+
+<!-- Choices.js JS -->
+<script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
