11c23fb7f8c19857e5e6fd88fee83b9ffa291783
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-10 15:43:35 +0500

    c

--- resources/views/Frontend/hero_section.blade.php
+++ resources/views/Frontend/hero_section.blade.php
@@ -154,9 +154,8 @@
     font-size: 16px;
 
 }
-.select2-container--default .select2-selection--single,
-.select2-container--default .select2-selection--multiple {
-    border-radius: 4px;
+.select2-selection--single,
+.select2-selection--multiple {
     height: 40px;
     padding: 5px 10px;
 }
@@ -301,7 +300,6 @@
 .select2-selection__rendered {
     line-height: 45px !important;
 }
-
 .select2-selection__arrow {
     height: 45px !important;
 }
