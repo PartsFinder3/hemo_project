168ea72d636727370c4adb4bc7525f5900da0d5f
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-10 18:51:22 +0500

    c

--- resources/views/Frontend/layout/head.blade.php
+++ resources/views/Frontend/layout/head.blade.php
@@ -69,9 +69,7 @@
         @foreach ($scripts as $script) {!! $script->script_content !!} @endforeach
     @endif
     
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
-<script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
-   @yield('head-section')
+  @yield('head-section')
 </head>
 
 <body>
