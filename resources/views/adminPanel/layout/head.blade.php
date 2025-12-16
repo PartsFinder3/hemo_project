74371d19b4749b94284848b7c3575dddda3b523d
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-16 19:32:28 +0500

    c

--- resources/views/adminPanel/layout/head.blade.php
+++ resources/views/adminPanel/layout/head.blade.php
@@ -10,7 +10,7 @@
 @endif 
     {{-- Favicons --}}
     <link rel="shortcut icon" href="https://partsfinder.ae/storage/logo/44444.png" type="image/x-icon">
-
+  <link rel="icon" href="https://partsfinder.ae/storage/logo/44444.png">
     {{-- Fonts / Icons --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <!-- Summernote CSS -->
