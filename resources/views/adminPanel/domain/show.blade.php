14ab180c5e20fdece1d170c3d16e26ff02f91dfe
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-17 16:30:39 +0500

    c

--- resources/views/adminPanel/domain/show.blade.php
+++ resources/views/adminPanel/domain/show.blade.php
@@ -116,7 +116,7 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <table class="table table-striped" id="table1">
+                    <table class="table table-striped" >
                         <thead>
                             <tr>
                                 <th>Country</th>
