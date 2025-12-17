dff3dd7719a204bd19c5ef9b812d318877e4c7af
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-17 16:58:51 +0500

    c

--- resources/views/Frontend/layout/head.blade.php
+++ resources/views/Frontend/layout/head.blade.php
@@ -68,8 +68,8 @@
     @if ($scripts->count() > 0)
         @foreach ($scripts as $script) {!! $script->script_content !!} @endforeach
     @endif
-   <link rel="icon" href="{{ asset(domainFavicon()) }}" type="image/png">
-<link rel="shortcut icon" href="{{ asset(domainLogo()) }}" type="image/png">
+       <link rel="icon" href="https://partsfinder.ae/storage/logo/44444.png">
+       <link rel="shortcut icon" href="{{ asset('storage/logo/44444.png') }}" type="image/png">
   @yield('head-section')
 </head>
 
