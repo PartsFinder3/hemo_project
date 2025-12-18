5a1057e4e943bedef6baf2adee5a01ee035de7e4
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-18 14:12:44 +0500

    c

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -75,7 +75,7 @@ public function index(Request $request)
         ->where('domain', $host)
         ->latest()
         ->paginate(8);
-    dd($host);
+    dd($currentDomain);
     return view('Frontend.index', compact(
         'carMakes',
         'domain',
