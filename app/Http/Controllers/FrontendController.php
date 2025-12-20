357680a8acc056578466bd6c7863d51b6dd27473
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-20 16:37:52 +0500

    c

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -485,7 +485,7 @@ public function sendProductInquiry(Request $request)
 
     public function viewAd($slug, $id)
     {
-        $ad = Ads::where('id', $id)->firstOrFail();
+        $ad = Ads::where('id', $id)->where('slug', $slug)->firstOrFail();
         return view('Frontend.view-add', compact('ad'));
     }
 
