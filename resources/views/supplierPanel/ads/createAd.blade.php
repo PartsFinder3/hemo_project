e82e2742b35b73c48600bebda5466072f611eab2
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-21 11:53:43 +0500

    c

--- resources/views/supplierPanel/ads/createAd.blade.php
+++ resources/views/supplierPanel/ads/createAd.blade.php
@@ -6,9 +6,6 @@
     }
 </style>
 
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
 @section('main-section')
     <div class="container mt-5">
         <div class="row">
--- resources/views/supplierPanel/layout/head.blade.php
+++ resources/views/supplierPanel/layout/head.blade.php
@@ -14,6 +14,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <!-- Summernote CSS -->
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.css" rel="stylesheet">
+<!-- supplierPanel.layout.main ke head me -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
     {{-- Vendor CSS --}}
     <link rel="stylesheet" href="{{ asset('assets/extensions/simple-datatables/style.css') }}">
