db461be8a3933dd74ce7d07fd9b9b5f3f2715761
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-21 11:50:33 +0500

    c

--- resources/views/supplierPanel/ads/createAd.blade.php
+++ resources/views/supplierPanel/ads/createAd.blade.php
@@ -1,3 +1,7 @@
+
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 @extends('supplierPanel.layout.main')
 <style>
     .mb-3 label{
