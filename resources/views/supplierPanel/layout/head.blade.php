4d42913136d8c5ce8fd5f8814c81c07568e7d907
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-21 11:55:17 +0500

    c

--- resources/views/supplierPanel/layout/head.blade.php
+++ resources/views/supplierPanel/layout/head.blade.php
@@ -17,6 +17,7 @@
 <!-- supplierPanel.layout.main ke head me -->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     {{-- Vendor CSS --}}
     <link rel="stylesheet" href="{{ asset('assets/extensions/simple-datatables/style.css') }}">
