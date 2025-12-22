e7e5ab462180b753160dc9320d5555185431cf2d
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-22 14:17:54 +0500

    c

--- resources/views/supplierPanel/ads/createAd.blade.php
+++ resources/views/supplierPanel/ads/createAd.blade.php
@@ -1,13 +1,5 @@
 
 @extends('supplierPanel.layout.main')
-<!-- jQuery FIRST -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<!-- Select2 CSS -->
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
-
-<!-- Select2 JS -->
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <style>
     .mb-3 label{
