0dc60448ca4062fe65e6f08d7ca853bee5e0e954
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-24 21:46:31 +0500

    c

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,8 @@ class VerifyCsrfToken extends Middleware
      */
    protected $except = [
         'buyers/inquiry/send',
+        'partsfinderoman.ae',
+        'https://partsfinder.ge',
         '/partsfinder/public/buyers/inquiry/send'
     ];
 }
