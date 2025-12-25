532a89b3b1c67cb0f401593936fe114c4dc5b26e
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-25 19:31:55 +0500

    cc

--- resources/views/Analytics/united.blade.php
+++ resources/views/Analytics/united.blade.php
@@ -243,7 +243,7 @@
             <h3 class="header-title fw-bold mb-2">Domain : {{$thisdomain}}</h3>
             <div class="d-flex align-items-center gap-3">
                 <div class="country-badge">
-                    <i class="bi bi-geo-alt" style="transform: translateY(-2px);"></i>
+                    <i class="bi bi-geo-alt" style="transform: translateY(-10px);"></i>
                     @php
                     use App\Models\Domain;
                          $currentDomain = Domain::where('domain_url', $thisdomain)->first();
