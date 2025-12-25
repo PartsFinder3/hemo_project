736e618abca44401fc37e4ae342219467d998189
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-25 19:57:22 +0500

    c

--- resources/views/Frontend/allmake.blade.php
+++ resources/views/Frontend/allmake.blade.php
@@ -19,8 +19,6 @@
                 </a>
             @endforeach
         </div>
-        <div class="view-all mt-4 text-center">
-        <a href="{{ route('all.makes') }}" class="btn btn-primary px-4 py-2">View All</a>
-    </div>
+        
     </section>
 @endsection
