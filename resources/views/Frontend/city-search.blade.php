6a838bd665f9afaa5ab88553c93a95b6a3cffb01
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-26 21:44:26 +0500

    c

--- resources/views/Frontend/city-search.blade.php
+++ resources/views/Frontend/city-search.blade.php
@@ -285,61 +285,7 @@
         </div>
     </section>
 
-    <section class="ad-cards">
-        <div class="section-text">
-            <h3>{{ $city->name }} ADS</h3>
-            <h2>Find the Best Deals For You</h2>
-        </div>
-        <div class="filters">
-            <a href="#" class="active">All</a>
-            @foreach ($randomParts as $p)
-                <a href="#">{{ $p->name }}</a>
-            @endforeach
-        </div>
-
-    <div class="grid" id="productGrid1">
-        <!-- Example Card -->
-        @foreach ($ads as $ad)
-            <div class="card">
-                @php
-                    $images = json_decode($ad->images, true);
-                @endphp
-
-                @if(!empty($images[0]))
-                    <img src="{{ asset($images[0]) }}" class="card-img-top img-fluid" alt="Product">
-                @endif
-                
-                <div class="card-body">
-                    <a href="{{ route('view.ad', ['slug' => Str::slug($ad->title), 'id' => $ad->id]) }}"
-                        class="card-title">{{ $ad->title }}</a>
-                    <div class="price">{{ $ad->currency }} {{ $ad->price }}</div>
-                    <div class="meta">
-                        Availability: In Stock <br>
-                        Condition: {{ $ad->condition }} <br>
-                        Delivery: Ask Supplier <br>
-                        Warranty: Ask Supplier
-                    </div>
-                    
-                    <div class="buttons">
-                        <a href="https://wa.me/{{ preg_replace('/\D/', '', $ad->shop->supplier->whatsapp) }}?text={{ urlencode('Hello, I am interested in your ad: ' . $ad->title) }}"
-                            target="_blank"
-                            class="btn btn-sm btn-success w-100 my-1">
-                            <i class="fab fa-whatsapp me-1"></i> WhatsApp
-                        </a>
-                        
-                        <a href="javascript:void(0)" class="btn call"
-                            onclick="callSupplier('{{ $ad->shop->supplier->is_active }}', '{{ $ad->shop->supplier->whatsapp }}')">
-                            <i class="fa-solid fa-phone"></i> Click to Call
-                        </a>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    </div>
-
-        <div class="pagination" id="pagination1"></div>
-    </section>
-
+ 
         <section class="ad-cards">
         <div class="section-text">
             <h3>{{$city->name}} CAR ADS</h3>
