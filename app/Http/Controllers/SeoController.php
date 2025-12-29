e3cd715784cf833cd11194f1a90059339de4eab5
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-29 12:43:47 +0500

    c

--- app/Http/Controllers/SeoController.php
+++ app/Http/Controllers/SeoController.php
@@ -116,7 +116,7 @@ class SeoController extends Controller
             $getTamp = $parts->tamp_id 
                 ? SeoTamplate::find($parts->tamp_id) 
                 : null;
-
+           dd($getTamp);
             // All description templates
             $allTemplte = SeoTamplate::where('type', 'city')->get();
 
