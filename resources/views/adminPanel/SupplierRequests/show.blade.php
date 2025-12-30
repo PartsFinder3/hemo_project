4e66f5595b3ddec08452cbc274b4de10a34a7452
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-30 21:03:39 +0500

    c

--- resources/views/adminPanel/SupplierRequests/show.blade.php
+++ resources/views/adminPanel/SupplierRequests/show.blade.php
@@ -178,6 +178,7 @@
         </div>
     </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
         function contactSupplier(whatsapp) {
