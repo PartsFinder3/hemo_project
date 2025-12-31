2f7c2d3a202a6a1c012215e536cbb9eb2c02179b
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-31 14:35:01 +0500

    c

--- resources/views/Frontend/city-search.blade.php
+++ resources/views/Frontend/city-search.blade.php
@@ -262,6 +262,12 @@
     height: 200px;
     object-fit: contain;
 }
+.hero_section_text {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 3px;
+     z-index: 9999; 
+}
 </style>
 @include('Frontend.hero_section', [
    'part' => '<span class="hiliter" >' . $city->name . '</span> Parts for Sale in UAE',
