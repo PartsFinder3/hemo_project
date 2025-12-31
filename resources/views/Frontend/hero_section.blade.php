73da01b9a26038dfb807b68033a4392794e7cd9a
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-31 13:55:39 +0500

    c

--- resources/views/Frontend/hero_section.blade.php
+++ resources/views/Frontend/hero_section.blade.php
@@ -100,7 +100,7 @@
     justify-content: flex-start;  /* ✅ left align */
     align-items: center;          /* optional */
     margin-top: 20px;
-   
+    position: absolute;
     text-align: left;             /* center hatao */
 }
 .hero_image_section img {
