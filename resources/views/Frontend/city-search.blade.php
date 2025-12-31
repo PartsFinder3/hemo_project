465590f9fe86446d87cc7a5fce4d1df97ef8b5a8
AbdulDev9393 <abduldeveloper701@gmail.com>
2025-12-31 14:33:00 +0500

    c

--- resources/views/Frontend/city-search.blade.php
+++ resources/views/Frontend/city-search.blade.php
@@ -264,7 +264,7 @@
 }
 </style>
 @include('Frontend.hero_section', [
-   'part' => '<span class="hiliter">' . $city->name . '</span> Parts for Sale in UAE',
+   'part' => '<span class="hiliter" >' . $city->name . '</span> Parts for Sale in UAE',
    'image' => 'https://partsfinder.ae/storage/profile_images/hero_section_image_1.png'
 ])
 
