3e0d423a38281058ed4f0d04f985ebec6ee4441b
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-01-03 11:43:39 +0500

    c

--- resources/views/ReviewPage.blade.php
+++ resources/views/ReviewPage.blade.php
@@ -162,7 +162,7 @@
     
     .section-title {
         font-size: 2.5rem;
-        color: black;
+        color: black !important;
         margin-bottom: 10px;
         font-weight: 700;
         letter-spacing: -0.5px;
