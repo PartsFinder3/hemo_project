8d5b3a1e770246ea00f3a9bb64964d93f152af4e
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-01-03 14:50:57 +0500

    c

--- resources/views/Frontend/layout/head.blade.php
+++ resources/views/Frontend/layout/head.blade.php
@@ -96,6 +96,8 @@
 <link rel="icon"  href="{{ asset($logoPath) }}?v={{ time() }}"  type="{{ $faviconMime }}">
 <link rel="shortcut icon" href="{{ asset($logoPath) }}?v={{ time() }}" type="{{ $faviconMime }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E6ZGW2V8LH"></script>
+<script defer src="https://scripts.clarity.ms/js/clarity.js"></script>
 <meta property="og:image" content="{{ asset('storage/'.$logo) }}">
   @yield('head-section')
 </head>
