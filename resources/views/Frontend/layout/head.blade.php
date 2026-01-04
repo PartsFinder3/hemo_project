f8fb5f50ec962e213a89ffc842a13f00c4d9b2ea
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-01-04 13:43:49 +0500

    c

--- resources/views/Frontend/layout/head.blade.php
+++ resources/views/Frontend/layout/head.blade.php
@@ -69,7 +69,7 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- Include Select2 CSS & JS -->
        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <link rel="stylesheet" href="{{ asset('Frontend/css/style.css') }}">
