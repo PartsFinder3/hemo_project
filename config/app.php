04983989c865e377aa442175c536a577d5a037a4
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-01-04 13:22:54 +0500

    c

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+          Intervention\Image\ImageServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -181,7 +182,7 @@ return [
     |
     */
 
-'aliases' => Facade::defaultAliases()->merge([
+'aliases' => [
+    // Other aliases...
     'Image' => Intervention\Image\Facades\Image::class,
-])->toArray(),
-];
+],
