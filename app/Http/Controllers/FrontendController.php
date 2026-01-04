576594cb9d8ef43d2489be570d3b6b9956405028
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-01-04 12:45:22 +0500

    c

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -38,7 +38,7 @@ use App\Services\InquiryService;
 use App\Jobs\GenerateSeoContentJob;
 use function Ramsey\Uuid\v1;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Image;
+use Intervention\Image\Facades\Image;
 use Illuminate\Support\Str;
 
 class FrontendController extends Controller
--- config/app.php
+++ config/app.php
@@ -181,7 +181,7 @@ return [
     |
     */
 
-  'aliases' => Facade::defaultAliases()->merge([
-    'Image' =>Intervention\Image\Image::class,
+'aliases' => Facade::defaultAliases()->merge([
+    'Image' => Intervention\Image\Facades\Image::class,
 ])->toArray(),
 ];
