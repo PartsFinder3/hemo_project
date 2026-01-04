5826d1d97844de23231302a99b97543629bd94ed
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-01-04 13:46:06 +0500

    c

--- resources/views/Frontend/layout/head.blade.php
+++ resources/views/Frontend/layout/head.blade.php
@@ -62,7 +62,11 @@
           href="{{ asset('storage/profile_images/hero_section_image_1.png') }}" 
           fetchpriority="high">
 
-
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap"
+        rel="stylesheet" />
     <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
